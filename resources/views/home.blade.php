3bf65f2e5bca70e42c1b3ceb87682cf11bbf1b03
David <david.juanaranda@gmail.com>
2020-11-18 15:22:02 +0100

    Algunos cambios realizados en archivos para que funcione en UNIX

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prueba</title>
+	<link rel="stylesheet" type="text/css" href="{{asset('css/styles.css')}}">
+</head>
+<body>
+<div class="form">
+  	<form action="/action_page.php">
+    <input type="text" id="title" name="title" placeholder="Title">
+
+    <input type="text" id="description" name="description" placeholder="Crear la nota...">
+
+    <input type="submit" value="Crear">
+  	</form>
+</div>
+</body>
+</html>
